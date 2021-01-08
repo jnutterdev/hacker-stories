0ee1e862c9546def060fa38123a0f67db04282a5
jnutterdev <hello@jnutterdev.com>
2021-01-07 23:10:28 -0500

    stopped on page. 26

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-function getTitle(title) {
-  return title;
-}
 
 const list = [
   {
@@ -23,21 +20,35 @@ const list = [
   },
 ];
 
-function App() {
-
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
-
-  return (
-    <div>
-      <h1>Hello, {getTitle('React')}</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text"/>
-    </div>
-  );
+function List() {
+  return list.map(function(item) {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href="{item.url}">{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.num_points}</span>
+      </div>
+    );
+  });
 }
+
+function App() {
+return (
+<div>
+  <h1>My Hacker Stories</h1>
+
+  <label htmlFor="search">Search: </label>
+  <input id="search" type="text"/>
+
+  <hr/>
+  <List />
+</div>
+)
+
+  // closing App bracket
+};
 
 export default App;
